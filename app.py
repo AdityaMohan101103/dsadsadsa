d8f97abbf6b1b68ea55d7ca6b990e1ac9d944a42
AdityaMohan101103 <157048346+AdityaMohan101103@users.noreply.github.com>
2025-06-13 16:02:37 +0530

    Update app.py

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.chrome.options import Options
 from webdriver_manager.chrome import ChromeDriverManager
+#dewfwfwefewfwe
 from selenium.webdriver.common.by import By
 from selenium.common.exceptions import NoSuchElementException
 import time
